338aa89d4363e1871b680f9140df26aa98cd2e11
MinexAutomation <minex.automation@gmail.com>
2020-01-23 12:29:45 -0800

    Added documentation.

--- source/R5T.Megara.Stockholm.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Megara.Stockholm.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -9,6 +9,9 @@ namespace R5T.Megara.Stockholm.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="IFileSerializer{T}"/> service implmentation.
+        /// </summary>
         public static IServiceCollection AddStreamFileSerializer<T>(this IServiceCollection services)
         {
             services.AddSingleton<IFileSerializer<T>, StreamFileSerializer<T>>();
@@ -16,6 +19,9 @@ namespace R5T.Megara.Stockholm.Standard
             return services;
         }
 
+        /// <summary>
+        /// Adds the <see cref="IFileSerializer{T}"/> and the <typeparamref name="TStreamSerializer"/> <see cref="IStreamSerializer{T}"/> implementation.
+        /// </summary>
         public static IServiceCollection AddStreamFileSerializer<T, TStreamSerializer>(this IServiceCollection services)
             where TStreamSerializer: class, IStreamSerializer<T>
         {
