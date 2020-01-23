a149a6d9a7f937bf86f787529a1ee1e687defcde
MinexAutomation <minex.automation@gmail.com>
2020-01-23 12:22:08 -0800

    Added IServiceCollection extensions.

--- source/R5T.Megara.Stockholm.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Megara.Stockholm.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Megara.Stockholm.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Megara.Stockholm.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddStreamFileSerializer<T>(this IServiceCollection services)
+        {
+            services.AddSingleton<IFileSerializer<T>, StreamFileSerializer<T>>();
+
+            return services;
+        }
+    }
+}
