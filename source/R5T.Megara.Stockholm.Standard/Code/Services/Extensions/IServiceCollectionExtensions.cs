154e9a355e524812fd810e1f41e599c055bad4b3
MinexAutomation <minex.automation@gmail.com>
2020-01-23 12:27:45 -0800

    Added TStreamSerializer-guided AddStreamFileSerializer<T, TStreamSerializer>() method.

--- source/R5T.Megara.Stockholm.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Megara.Stockholm.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Stockholm;
+
 
 namespace R5T.Megara.Stockholm.Standard
 {
@@ -10,6 +12,17 @@ namespace R5T.Megara.Stockholm.Standard
         public static IServiceCollection AddStreamFileSerializer<T>(this IServiceCollection services)
         {
             services.AddSingleton<IFileSerializer<T>, StreamFileSerializer<T>>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddStreamFileSerializer<T, TStreamSerializer>(this IServiceCollection services)
+            where TStreamSerializer: class, IStreamSerializer<T>
+        {
+            services
+                .AddStreamFileSerializer<T>()
+                .AddSingleton<IStreamSerializer<T>, TStreamSerializer>()
+                ;
 
             return services;
         }
